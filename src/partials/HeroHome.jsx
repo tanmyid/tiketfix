2ed36569f94fa441f144a300196388f28b1baec2
ehroy <ahmadroy092@gmail.com>
2023-03-09 01:10:26 +0700

    update

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -1,20 +1,55 @@
-import React, { useState } from 'react';
-import Modal from '../utils/Modal';
+import React, { useState } from "react";
+import Modal from "../utils/Modal";
+import HeroImage from "../images/roadmap.webp";
+import Carousel from "react-multi-carousel";
+import "../css/App.css";
+import ImageSlider from "../utils/Imageslider";
+import { SliderData } from "../utils/Sliderdata";
 
-import HeroImage from '../images/roadmap.webp';
-
+// import "react-multi-carousel/lib/styles.css";
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 function HeroHome() {
-
   const [videoModalOpen, setVideoModalOpen] = useState(false);
 
   return (
     <section className="relative">
-
       {/* Illustration behind hero content */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none" aria-hidden="true">
-        <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
+      <div
+        className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none"
+        aria-hidden="true"
+      >
+        <svg
+          width="1360"
+          height="578"
+          viewBox="0 0 1360 578"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <defs>
-            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
+            <linearGradient
+              x1="50%"
+              y1="0%"
+              x2="50%"
+              y2="100%"
+              id="illustration-01"
+            >
               <stop stopColor="#FFF" offset="0%" />
               <stop stopColor="#EAEAEA" offset="77.402%" />
               <stop stopColor="#DFDFDF" offset="100%" />
@@ -28,21 +63,48 @@ function HeroHome() {
       </div>
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
         {/* Hero content */}
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Management Expo 2023 <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">"Sinergikan UMKM Dalam Mahakarya Budaya" </span></h1>
+            <h1
+              className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
+              data-aos="zoom-y-out"
+            >
+              Management Expo 2023{" "}
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
+                "Sinergikan UMKM Dalam Mahakarya Budaya"{" "}
+              </span>
+            </h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">"Kesempatan untuk menjelajahi suasana baru hanya ada di #managementexpo2023 Datang dan temukan inspirasi terbaikmu!"</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
+              <p
+                className="text-xl text-gray-600 mb-8"
+                data-aos="zoom-y-out"
+                data-aos-delay="150"
+              >
+                "Kesempatan untuk menjelajahi suasana baru hanya ada di
+                #managementexpo2023 Datang dan temukan inspirasi terbaikmu!"
+              </p>
+              <div
+                className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center"
+                data-aos="zoom-y-out"
+                data-aos-delay="300"
+              >
                 <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://www.instagram.com/managementexpo.unisnu/">Management Expo</a>
+                  <a
+                    className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0"
+                    href="https://www.instagram.com/managementexpo.unisnu/"
+                  >
+                    Management Expo
+                  </a>
                 </div>
                 <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="https://www.tiktok.com/@managementexpo23">Management Expo</a>
+                  <a
+                    className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4"
+                    href="https://www.tiktok.com/@managementexpo23"
+                  >
+                    Management Expo
+                  </a>
                 </div>
               </div>
             </div>
@@ -50,56 +112,54 @@ function HeroHome() {
 
           {/* Hero image */}
           <div>
-            <div className="relative flex justify-center mb-8" data-aos="zoom-y-out" data-aos-delay="450">
-              <div className="flex flex-col justify-center">
-                <img className="mx-auto" src={HeroImage} width="768" height="432" alt="Hero" />
-                <svg className="absolute inset-0 max-w-full mx-auto md:max-w-none h-auto" width="768" height="432" viewBox="0 0 768 432" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-                  <defs>
-                    <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="hero-ill-a">
-                      <stop stopColor="#FFF" offset="0%" />
-                      <stop stopColor="#EAEAEA" offset="77.402%" />
-                      <stop stopColor="#DFDFDF" offset="100%" />
-                    </linearGradient>
-                    <linearGradient x1="50%" y1="0%" x2="50%" y2="99.24%" id="hero-ill-b">
-                      <stop stopColor="#FFF" offset="0%" />
-                      <stop stopColor="#EAEAEA" offset="48.57%" />
-                      <stop stopColor="#DFDFDF" stopOpacity="0" offset="100%" />
-                    </linearGradient>
-                    <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="hero-ill-e">
-                      <stop stopColor="#4FD1C5" offset="0%" />
-                      <stop stopColor="#81E6D9" offset="25.871%" />
-                      <stop stopColor="#338CF5" offset="100%" />
-                    </radialGradient>
-                  </defs>
-                </svg>
-              </div>
-             
-              <button className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg" onClick={(e) => { e.preventDefault(); e.stopPropagation(); setVideoModalOpen(true); }} aria-controls="modal">
-                <svg className="w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 flex-shrink-0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <div
+              className="relative flex justify-center mb-8"
+              data-aos="zoom-y-out"
+              data-aos-delay="450"
+            >
+              <ImageSlider slides={SliderData} />
+
+              <button
+                className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg"
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  setVideoModalOpen(true);
+                }}
+                aria-controls="modal"
+              >
+                <svg
+                  className="w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 flex-shrink-0"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
                   <path d="M10 17l6-5-6-5z" />
                 </svg>
                 <span className="ml-3">Watch the full video (2 min)</span>
               </button>
-           
             </div>
 
             {/* Modal */}
-            <Modal id="modal" ariaLabel="modal-headline" show={videoModalOpen} handleClose={() => setVideoModalOpen(false)}>
+            <Modal
+              id="modal"
+              ariaLabel="modal-headline"
+              show={videoModalOpen}
+              handleClose={() => setVideoModalOpen(false)}
+            >
               <div className="relative pb-9/16">
-              <iframe className='video absolute w-full h-full'
-                      title='Video'
-                      sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
-                      src={`https://youtube.com/embed/kDeFktafwTQ?autoplay=0`} allowFullScreen>
-              </iframe>
+                <iframe
+                  className="video absolute w-full h-full"
+                  title="Video"
+                  sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
+                  src={`https://youtube.com/embed/kDeFktafwTQ?autoplay=0`}
+                  allowFullScreen
+                ></iframe>
                 {/* <iframe className="absolute w-full h-full" src="https://www.youtube.com/watch?v=QIpWKTHKcqM" title="Video" allowFullScreen></iframe> */}
               </div>
             </Modal>
-
           </div>
-
         </div>
-
       </div>
     </section>
   );
---
+++ src/utils/Sliderdata.jsx
@@ -0,0 +1,15 @@
+import Slider1 from "../images/roadmap.webp";
+import Slider2 from "../images/andi.webp";
+import Slider3 from "../images/logomanajemen.webp";
+
+export const SliderData = [
+  {
+    image: Slider1,
+  },
+  {
+    image: Slider2,
+  },
+  {
+    image: Slider3,
+  },
+];
