cdec049b975e1f8b1403ab9a8f8be0a463abd897
ehroy <ahmadroy092@gmail.com>
2023-03-09 02:40:03 +0700

    update

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -64,7 +64,7 @@ function HeroHome() {
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         {/* Hero content */}
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+        <div className="pt-32 pb-12 md:pt-20 md:pb-20">
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
             <h1
@@ -113,7 +113,7 @@ function HeroHome() {
           {/* Hero image */}
           <div>
             <div
-              className="relative flex justify-center mb-8"
+              className="relative flex justify-center mb-2"
               data-aos="zoom-y-out"
               data-aos-delay="450"
             >
--- src/utils/Imageslider.jsx
+++ src/utils/Imageslider.jsx
@@ -29,7 +29,60 @@ const ImageSlider = ({ slides }) => {
             key={index}
           >
             {index === current && (
-              <img src={slide.image} alt="travel image" className="image" />
+              <div>
+                <img
+                  src={slide.image}
+                  alt="banner image"
+                  className="image"
+                  width="768"
+                  height="432"
+                />
+                <svg
+                  className="absolute inset-0 max-w-full mx-auto md:max-w-none h-auto"
+                  width="768"
+                  height="432"
+                  viewBox="0 0 768 432"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                >
+                  <defs>
+                    <linearGradient
+                      x1="50%"
+                      y1="0%"
+                      x2="50%"
+                      y2="100%"
+                      id="hero-ill-a"
+                    >
+                      <stop stopColor="#FFF" offset="0%" />
+                      <stop stopColor="#EAEAEA" offset="77.402%" />
+                      <stop stopColor="#DFDFDF" offset="100%" />
+                    </linearGradient>
+                    <linearGradient
+                      x1="50%"
+                      y1="0%"
+                      x2="50%"
+                      y2="99.24%"
+                      id="hero-ill-b"
+                    >
+                      <stop stopColor="#FFF" offset="0%" />
+                      <stop stopColor="#EAEAEA" offset="48.57%" />
+                      <stop stopColor="#DFDFDF" stopOpacity="0" offset="100%" />
+                    </linearGradient>
+                    <radialGradient
+                      cx="21.152%"
+                      cy="86.063%"
+                      fx="21.152%"
+                      fy="86.063%"
+                      r="79.941%"
+                      id="hero-ill-e"
+                    >
+                      <stop stopColor="#4FD1C5" offset="0%" />
+                      <stop stopColor="#81E6D9" offset="25.871%" />
+                      <stop stopColor="#338CF5" offset="100%" />
+                    </radialGradient>
+                  </defs>
+                </svg>
+              </div>
             )}
           </div>
         );
